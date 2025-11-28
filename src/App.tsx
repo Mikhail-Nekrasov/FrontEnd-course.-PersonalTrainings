619b051cd30b5d40f0014807f8e1d20721fd05ba
Mikhail Nekrasov <nekrasovm2@gmail.com>
2025-11-28 02:37:31 +0200

    Eighth commit. Trying to deploy on GitHub 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Router from './router';
 import Navbar from './components/Navbar';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
         <Navbar />
         <main style={{ flex: 1 }}>
           <Router />
         </main>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
